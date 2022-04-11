3ccd276859844f126db7c2bc521dd4352878dca0
Ben Weatherall <ben.weatherall@gmail.com>
2022-04-11 11:54:58 +0930

    [Feat] Added about-me and skills html building from json

--- README.md
+++ README.md
@@ -3,5 +3,12 @@
 Portfolio of Ben Weatherall; Software Developer currently residing in Adelaide, South Australia.
 
 ## Acknowledgement
-Derived from tutorial: [https://www.freecodecamp.org/news/how-to-build-a-developer-portfolio-website/]
-by Kolade Chris ([https://github.com/Ksound22/developer-portfolio/]).
+Website derived from tutorial: [https://www.freecodecamp.org/news/how-to-build-a-developer-portfolio-website/] by Kolade Chris ([https://github.com/Ksound22/developer-portfolio/]).
+
+
+# Icons
+C# Icon By Jason Groce, Public Domain, [https://commons.wikimedia.org/w/index.php?curid=21111649]
+C++ Icon By Jeremy Kratz, Public Domain, [https://github.com/isocpp/logos]
+Python Icon By Python Software Foundation, Licensed (Nominative Use), [https://www.python.org/psf/trademarks/]
+Unity Icon By Unity Technologies, Licensed (Permitted Use), [https://unity3d.com/legal/branding_trademarks]
+OpenGL Icon By ™/®Hewlett Packard Enterprise, used under licence by the Khronos Group, Vectorised by Vulphere, Licensed (Permitted Use) [https://www.khronos.org/legal/trademarks/]
--- assets/js/app.js
+++ assets/js/app.js
@@ -33,7 +33,7 @@ navLink.forEach((link) =>
 function BuildLinkElement(linksObject){
   let res = ""; 
   linksObject.forEach((link) => {
-    res += `<a href="${link['link']}>${link['title']}</a></br>"`
+    res += `<a href=${link['link']}>${link['title']}</a></br>`
   }); 
   return res;
 }
@@ -43,40 +43,24 @@ function BuildProjectHighlights(path){
   $.getJSON(path, function(json) {
     json['projects'].forEach((project) => {
       if('highlight' in project){
-        $('#projects-container').append(
-        `<div class="all-project-container project-card">
-        <div class="project-card-left">
-        ${'image' in project ? `
+        $('#project-highlights').append(
+          `<div class="project-container project-card">
           <img
             src=${project['image']}
             alt=${project['alt-text']}
             loading="lazy"
-            class="all-project-pic"
-          />` : ``}  
-          <p>
-            <strong>Year: </strong>${project['start-year']}${project['on-going']? '-' : ''}</br>
-            <strong>Organisation: </strong>${project['organisation']}</br>
-            ${'languages' in project ? `
-            <strong>Languages: </strong> ${project['languages'].join(', ')}
-            </br>` : ``}
-            ${'technologies' in project ? `
-            <strong>Technologies: </strong> ${project['technologies'].join(', ')}
-            </br>` : ``}
-            ${'links' in project ? `
-            <strong>Links: </strong></br>
-            ${BuildLinkElement(project['links'])}` : ``}
+            class="project-pic"
+          />
+          <h3 class="project-title">${project['title']}</h3>
+          <p class="project-details">
+          ${project['brief']}
           </p>
-        </div>
-        <div class="project-card-right">
-          <h3 class="all-project-title">${project['title']}</h3>
-          <p class="all-project-details">
-          ${project['details']} 
-          </p>
-        </div>
-        </div>`);  
+          <a href="projects.html#${project['section-title']}" target="_blank" class="project-link">Check it Out</a>
+          </div>`
+        );  
       }
-    })
-  });  
+    });
+  });
 }
 
 function BuildProjects(path){
@@ -116,4 +100,72 @@ function BuildProjects(path){
       </div>`);  
     })
   });
+}
+
+function BuildAboutMe(path){
+  $('#about-me-brief').empty();
+  $('#about-me').empty();
+  
+  $.getJSON(path, function(json) {
+    $('#about-me-brief').append(`
+    <p>${json['about-brief']}</p>`);
+
+    $('#about-me').append(`
+    <h2>More About Me</h2>
+    <p>${json['about']}</p>`);
+  });
+}
+
+function BuildSkillsBrief(path){
+  $('#skills-brief').empty();
+  $.getJSON(path, function(json) {
+
+    iconsPerLine = 4;
+    // Sections
+    let lines = Math.ceil(json['skills-brief'].length / iconsPerLine);
+
+    let skillsHTML = ``;
+    let ordinal = '';
+    for(let lineIDX = 0; lineIDX < lines; lineIDX++){
+      switch(lineIDX){
+        case 0:
+          ordinal = 'first';
+          break;
+        case 1:
+          ordinal = 'second';
+          break;
+        case 2:
+          ordinal = 'third';
+          break;
+        case 3:
+          ordinal = 'fourth';
+          break;
+        default:
+          ordinal = "";
+      }
+
+      skillsHTML += `<div class="${ordinal}-set animate__animated animate__pulse">`;
+      for(let iconIDX = 0; iconIDX < iconsPerLine; iconIDX++){
+        skillIDX = lineIDX * iconsPerLine + iconIDX;
+        if(skillIDX >= json['skills-brief'].length){
+          break;
+        }
+        skillsHTML += `<img
+          src="assets/icons/skills/${json['skills-brief'][skillIDX].toLowerCase()}-icon.svg"
+          alt=""
+          loading="lazy"
+          class="icon icon-card"
+        />`
+      }
+      skillsHTML += `</div>`;
+    }
+
+    $('#skills-brief').append(skillsHTML);
+  });
+}
+
+function BuildMainPage(){
+  BuildProjectHighlights('./assets/json/projects.json');
+  BuildAboutMe('./assets/json/me.json');
+  BuildSkillsBrief('./assets/json/me.json');
 }
