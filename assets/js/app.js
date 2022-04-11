b9c59578494de8b9894c6e37611eaf570d107dba
Ben Weatherall <ben.weatherall@gmail.com>
2022-04-11 09:58:27 +0930

    [Feat] Added project html generation from file (should be easier to include more files this way) [Tweak] Moved old project files into tools folder

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,3 +29,91 @@ navLink.forEach((link) =>
     ul.classList.remove("show");
   })
 );
+
+function BuildLinkElement(linksObject){
+  let res = ""; 
+  linksObject.forEach((link) => {
+    res += `<a href="${link['link']}>${link['title']}</a></br>"`
+  }); 
+  return res;
+}
+
+function BuildProjectHighlights(path){
+  $('#project-highlights').empty();
+  $.getJSON(path, function(json) {
+    json['projects'].forEach((project) => {
+      if('highlight' in project){
+        $('#projects-container').append(
+        `<div class="all-project-container project-card">
+        <div class="project-card-left">
+        ${'image' in project ? `
+          <img
+            src=${project['image']}
+            alt=${project['alt-text']}
+            loading="lazy"
+            class="all-project-pic"
+          />` : ``}  
+          <p>
+            <strong>Year: </strong>${project['start-year']}${project['on-going']? '-' : ''}</br>
+            <strong>Organisation: </strong>${project['organisation']}</br>
+            ${'languages' in project ? `
+            <strong>Languages: </strong> ${project['languages'].join(', ')}
+            </br>` : ``}
+            ${'technologies' in project ? `
+            <strong>Technologies: </strong> ${project['technologies'].join(', ')}
+            </br>` : ``}
+            ${'links' in project ? `
+            <strong>Links: </strong></br>
+            ${BuildLinkElement(project['links'])}` : ``}
+          </p>
+        </div>
+        <div class="project-card-right">
+          <h3 class="all-project-title">${project['title']}</h3>
+          <p class="all-project-details">
+          ${project['details']} 
+          </p>
+        </div>
+        </div>`);  
+      }
+    })
+  });  
+}
+
+function BuildProjects(path){
+  $('#projects-container').empty();
+  $.getJSON(path, function(json) {
+    json['projects'].forEach((project) => {
+      $('#projects-container').append(
+      `<div class="all-project-container project-card">
+      <div class="project-card-left">
+      ${'image' in project ? `
+        <img
+          src=${project['image']}
+          alt=${project['alt-text']}
+          loading="lazy"
+          class="all-project-pic"
+        />` : ``}  
+        <p>
+          <strong>Year: </strong>${project['start-year']}${project['on-going']? '-' : ''}</br>
+          <strong>Organisation: </strong>${project['organisation']}</br>
+          ${'languages' in project ? `
+          <strong>Languages: </strong> ${project['languages'].join(', ')}
+          </br>` : ``}
+          ${'technologies' in project ? `
+          <strong>Technologies: </strong> ${project['technologies'].join(', ')}
+          </br>` : ``}
+          ${'links' in project ? `
+          <strong>Links: </strong></br>
+          ${BuildLinkElement(project['links'])}` : ``}
+        </p>
+      </div>
+      <div class="project-card-right">
+        <h3 class="all-project-title">${project['title']}</h3>
+        <p class="all-project-details">
+        ${project['details']} 
+        </p>
+      </div>
+      </div>`);  
+    })
+  });
+}
